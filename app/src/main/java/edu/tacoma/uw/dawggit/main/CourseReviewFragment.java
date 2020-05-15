1c69b13b40f29dd030eae0fe981eff85bca723d3
Minh Nguyen <tmnx@users.noreply.github.com>
2020-05-15 09:55:34 -0700

    Added in course review tab (not clickable yet). Fixed fragment refreshing issue.

--- app/src/main/java/edu/tacoma/uw/dawggit/main/CourseReviewFragment.java
+++ app/src/main/java/edu/tacoma/uw/dawggit/main/CourseReviewFragment.java
@@ -28,25 +28,24 @@ import java.net.URL;
 import java.util.List;
 
 import edu.tacoma.uw.dawggit.R;
-import edu.tacoma.uw.dawggit.forum.Forum;
+import edu.tacoma.uw.dawggit.course.Course;
+import edu.tacoma.uw.dawggit.course.CourseAddActivity;
+import edu.tacoma.uw.dawggit.course.CourseDB;
 import edu.tacoma.uw.dawggit.forum.ForumAddActivity;
-import edu.tacoma.uw.dawggit.forum.ForumDb;
-import edu.tacoma.uw.dawggit.forum.ForumDisplayActivity;
+import edu.tacoma.uw.dawggit.review.ReviewDB;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ForumFragment extends Fragment {
+public class CourseReviewFragment extends Fragment {
     private boolean mTwoPane;
-    private List<Forum> mForumList;
+    private List<Course> mCourseList;
     private RecyclerView mRecyclerView;
-    private ForumDb mForumDB;
+    private CourseDB mCourseDB;
 
-
-
-    public ForumFragment() {
+    public CourseReviewFragment() {
         // Required empty public constructor
     }
 
@@ -56,11 +55,10 @@ public class ForumFragment extends Fragment {
      * @return A new instance of fragment HomeFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ForumFragment newInstance() {
-        ForumFragment fragment = new ForumFragment();
+    public static CourseReviewFragment newInstance() {
+        CourseReviewFragment fragment = new CourseReviewFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
-
         return fragment;
     }
 
@@ -69,17 +67,16 @@ public class ForumFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
         }
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View ve = inflater.inflate(R.layout.fragment_forum, container, false);
-        mRecyclerView = ve.findViewById(R.id.item_list);
+        View ve = inflater.inflate(R.layout.fragment_course_review, container, false);
+        mRecyclerView = ve.findViewById(R.id.course_review_list);
         assert mRecyclerView != null;
         setupRecyclerView((RecyclerView) mRecyclerView);
-        Button addButton = ve.findViewById(R.id.new_post);
+        Button addButton = ve.findViewById(R.id.addReviewButton);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,9 +93,9 @@ public class ForumFragment extends Fragment {
                 getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-            mForumList = null;
-            if (mForumList == null) {
-                new CoursesTask().execute(getString(R.string.get_threads));
+            mCourseList = null;
+            if (mCourseList == null) {
+                new CoursesTask().execute(getString(R.string.get_courses));
             }
         }
         else {
@@ -106,11 +103,11 @@ public class ForumFragment extends Fragment {
                     "No network connection available. Displaying locally stored data",
                     Toast.LENGTH_SHORT).show();
 
-            if (mForumDB == null) {
-                mForumDB = new ForumDb(getActivity());
+            if (mCourseDB == null) {
+                mCourseDB = new CourseDB(getActivity());
             }
-            if (mForumList == null) {
-                mForumList = mForumDB.getThreads();
+            if (mCourseList == null) {
+                mCourseList = mCourseDB.getCourses();
                 setupRecyclerView(mRecyclerView);
 
             }
@@ -118,21 +115,30 @@ public class ForumFragment extends Fragment {
     }
 
 
+    /**
+     * Set up the recycler view to display a list of courses.
+     * @param recyclerView a recycler view to be added to
+     */
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        if(mForumList != null) {
-            recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter((HomeActivity) getActivity(), mForumList, mTwoPane));
+        if(mCourseList != null) {
+            recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter((HomeActivity) getActivity(),
+                                                                        mCourseList, mTwoPane));
         }
     }
+
+    /**
+     * Helper class to manager the recycler view
+     */
     public static class SimpleItemRecyclerViewAdapter
             extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
 
         private final HomeActivity mParentActivity;
-        private final List<Forum> mValues;
+        private final List<Course> mValues;
         private final boolean mTwoPane;
         private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Forum item = (Forum) view.getTag();
+                Course item = (Course) view.getTag();
                 if (mTwoPane) {
 
 //                    Bundle arguments = new Bundle();
@@ -143,16 +149,23 @@ public class ForumFragment extends Fragment {
 //                            .replace(R.id.item_detail_container, fragment)
 //                            .commit();
                 } else {
-                    Context context = view.getContext();
-                    Intent intent = new Intent(context, ForumDisplayActivity.class);
-                    intent.putExtra(ForumDisplayActivity.ARG_ITEM_ID, item);
-                    context.startActivity(intent);
+//                    Context context = view.getContext();
+//                    Intent intent = new Intent(context, CourseDisplayActivity.class);
+//                    intent.putExtra(CourseDisplayActivity.ARG_ITEM_ID, item);
+//                    context.startActivity(intent);
                 }
             }
         };
 
+        /**
+         * Initializes fields.
+         *
+         * @param parent parent activity
+         * @param items list of courses
+         * @param twoPane boolean value
+         */
         SimpleItemRecyclerViewAdapter(HomeActivity parent,
-                                      List<Forum> items,
+                                      List<Course> items,
                                       boolean twoPane) {
             mValues = items;
             mParentActivity = parent;
@@ -168,10 +181,10 @@ public class ForumFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(final ViewHolder holder, int position) {
-            holder.mIdView.setText(mValues.get(position).getTitle());
-            holder.mContentView.setText(mValues.get(position).getDate());
+            holder.mIdView.setText(mValues.get(position).getCourse_code());
+            holder.mContentView.setText(mValues.get(position).getTitle());
             holder.itemView.setTag(mValues.get(position));
-            holder.itemView.setOnClickListener(mOnClickListener);
+//            holder.itemView.setOnClickListener(mOnClickListener);
         }
 
         @Override
@@ -179,6 +192,9 @@ public class ForumFragment extends Fragment {
             return mValues.size();
         }
 
+        /**
+         * Helper class to hold a view (for each course item).
+         */
         class ViewHolder extends RecyclerView.ViewHolder {
             final TextView mIdView;
             final TextView mContentView;
@@ -190,7 +206,12 @@ public class ForumFragment extends Fragment {
             }
         }
     }
+
+    /**
+     * Helper class to manage remote DB connection and retrieve DB.
+     */
     private class CoursesTask extends AsyncTask<String, Void, String> {
+
         @Override
         protected String doInBackground(String... urls) {
             String response = "";
@@ -221,6 +242,7 @@ public class ForumFragment extends Fragment {
 
         }
 
+        @Override
         protected void onPostExecute(String s) {
             if (s.startsWith("Unable to")) {
                 Toast.makeText(getActivity().getApplicationContext(), "Unable to download" + s, Toast.LENGTH_SHORT)
@@ -231,27 +253,26 @@ public class ForumFragment extends Fragment {
                 JSONObject jsonObject = new JSONObject(s);
 
                 if (jsonObject.getBoolean("success")) {
-                    mForumList = Forum.parseCourseJson(
+                    mCourseList = Course.parseCourseJSON(
                             jsonObject.getString("names"));
-                    if (mForumDB == null) {
-                        mForumDB = new ForumDb(getActivity().getApplicationContext());
+                    if (mCourseDB == null) {
+                        mCourseDB = new CourseDB(getActivity().getApplicationContext());
                     }
 
                     // Delete old data so that you can refresh the local
                     // database with the network data.
-                    mForumDB.deleteCourses();
+                    mCourseDB.deleteCourses();
 
                     // Also, add to the local database
-                    for (int i=0; i<mForumList.size(); i++) {
-                        Forum forum = mForumList.get(i);
-                        mForumDB.insertForum(forum.getThreadId(),
-                                forum.getTitle(),
-                                forum.getContent(),
-                                forum.getDate(),
-                                forum.getEmail());
+                    for (int i = 0; i< mCourseList.size(); i++) {
+                        Course course = mCourseList.get(i);
+                        mCourseDB.insertCourse(course.getCourse_code(),
+                                course.getTitle(),
+                                course.getCourse_info(),
+                                course.getEmail());
                     }
 
-                    if (!mForumList.isEmpty()) {
+                    if (!mCourseList.isEmpty()) {
                         setupRecyclerView((RecyclerView) mRecyclerView);
                     }
                 }
@@ -264,8 +285,11 @@ public class ForumFragment extends Fragment {
 
     }
 
+    /**
+     * Launch add course activity when user click add course button.
+     */
     private void launch() {
-        Intent intent = new Intent(getActivity(), ForumAddActivity.class);
+        Intent intent = new Intent(getActivity(), CourseAddActivity.class);
         startActivity(intent);
     }
 
